1f364f782f7e73b32d7dddc2c3604e29e00bc216
mori <mori@gmail.com>
2023-05-27 01:26:50 +0900

    creat network

--- main.tf
+++ main.tf
@@ -8,3 +8,15 @@ terraform {
     encrypt        = true
   }
 }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = local.region
+}
