5740288f1b4d329d3f023e92452ae43169cf1429
mori <mori@gmail.com>
2023-05-27 20:07:09 +0900

    use url variable

--- locals.tf
+++ locals.tf
@@ -1,6 +1,7 @@
 locals {
-  name   = replace(basename(path.cwd), "_", "-")
-  region = "ap-northeast-1"
-  app    = "go-simple-server"
-  url    = "d-wiki.com"
+  name    = replace(basename(path.cwd), "_", "-")
+  region  = "ap-northeast-1"
+  app     = "go-simple-server"
+  url     = "d-wiki.com"
+  stg_url = "stg.d-wiki.com"
 }
